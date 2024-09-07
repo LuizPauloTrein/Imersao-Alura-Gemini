28875f76e88a9f736a96d77e8aa40bd8964ad35c
Luiz <132777602+LuizPauloTrein@users.noreply.github.com>
2024-09-07 00:04:21 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo: "Mestre Yoda",
+        descricao: "Mestre Yoda, um Grão-Mestre Jedi de extraordinária sabedoria e poder, serviu à Ordem Jedi por séculos. Sua pequena estatura contrasta com sua imensa sabedoria, acumulada através de inúmeras experiências. Yoda é conhecido por seus ensinamentos profundos sobre a Força e por sua habilidade em guiar seus aprendizes para alcançar o equilíbrio e a iluminação.",
+        link: "https://pt.wikipedia.org/wiki/Yoda",
+        tags: "mestre, jedi, sabedoria"
+    },
+    
+    {
+        titulo: "Obi-Wan Kenobi",
+        descricao: "Um dos mais renomados Cavaleiros Jedi da Ordem, Obi-Wan Kenobi é conhecido por sua sabedoria, compaixão e habilidades com o sabre de luz. Como Mestre Jedi, treinou AnakinSkywalker e lutou em diversas batalhas, incluindo as Guerras Clônicas. Sua lealdade à Ordem Jedi e à República sempre foi inabalável.",
+        link: "https://pt.wikipedia.org/wiki.org/wiki/Obi-Wan_Kenobi",
+        tags: "jedi, cavaleiro, guerras, clonicas"
+    },
+    
+    {
+        titulo: "Anakin Skywalker",
+        descricao: "O Escolhido da profecia, Anakin Skywalker era um poderoso Jedi com um futuro brilhante pela frente. No entanto, influenciado pelo Lado Negro da Força, ele se tornou o temível Darth Vader. Sua jornada é marcada por conflitos internos, amor, perda e um destino trágico.",
+        link: "https://pt.wikipedia.org/wiki/Anakin_Skywalker",
+        tags: "sombrio, medo, raiva"
+    }
+];
